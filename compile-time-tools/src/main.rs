6e7a3498c350900290ec6666993ecae4417d7e8a
Stojan Ragnvlad <stojanragnvlad@gmail.com>
2024-04-29 20:04:47 -0600

    Creating the first version of the main rust file.

--- compile-time-tools/src/main.rs
+++ compile-time-tools/src/main.rs
@@ -1,3 +1,17 @@
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
+use qrcode_generator::QrCodeEcc;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let chars: String =
+        (0..24).map(|_| rng.sample(Alphanumeric) as char).collect();
+    println!("Random chars: {}", chars);
+
+    let result = qrcode_generator::to_png_to_file(
+        &chars,
+        QrCodeEcc::Low,
+        1024,
+        "./qr-code.png"
+    ).unwrap();
 }
