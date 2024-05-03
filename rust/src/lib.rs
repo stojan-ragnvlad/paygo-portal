0d1e1f4e43e3dd9320482856d625aa0f05d9b047
Stojan Ragnvlad <stojanragnvlad@gmail.com>
2024-05-03 11:34:38 -0600

    Moving all the code from the main file to the lib file.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,6 @@
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
+//use qrcode_generator::QrCodeEcc;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -6,7 +9,19 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn greet() {
+  let mut rng = thread_rng();
+  let chars: String =
+      (0..24).map(|_| rng.sample(Alphanumeric) as char).collect();
+  println!("Random chars: {}", chars);
+
+  //let result = qrcode_generator::to_png_to_file(
+    //  &chars,
+    // QrCodeEcc::Low,
+    //  1024,
+    //  "./qr-code.png"
+  //).unwrap();
+
+  alert(&format!("Hello, {}!", chars));
 }
 
