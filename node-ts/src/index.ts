220cab4f11aea2ecc463b502f7799b081dd1ab99
Stojan Ragnvlad <stojanragnvlad@gmail.com>
2024-05-02 21:26:18 -0600

    Getting a server up and running!

--- node-ts/src/index.ts
+++ node-ts/src/index.ts
@@ -1,1 +1,11 @@
- 
+import express from 'express';
+import { Request, Response } from 'express';
+
+const server = express();
+
+server.get('/', (request: express.Request, response: express.Response) => {
+  response.send('Hello World!');
+});
+
+server.listen(80);
+
