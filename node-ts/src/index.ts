7710cc021c3bd3b84e1a4a0bbbc3132f9f1cb0fa
Stojan Ragnvlad <stojanragnvlad@gmail.com>
2024-05-03 06:50:28 -0600

    Using the public directory to host static files.

--- node-ts/src/index.ts
+++ node-ts/src/index.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 
 const server = express();
 
+server.use(express.static('public'));
+
 server.get('/', (request: express.Request, response: express.Response) => {
   response.send('Hello World!');
 });
