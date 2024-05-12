e0bc96ba8a66a44d3a7f49f796dc544b74510d56
Stojan Ragnvlad <stojanragnvlad@gmail.com>
2024-05-11 19:28:50 -0600

    Creating the ng on init function.

--- angular/web-browser-client-angular/src/app/app.component.ts
+++ angular/web-browser-client-angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import init, { greet } from 'compile-time-tools/rust';
+// import wasmData, { greet } from 'compile-time-tools/rust_bg.wasm';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +13,10 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit{
   title = 'web-browser-client-angular';
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    fetch('rust_bg.wasm').then(response => response.arrayBuffer())
+      .then(bytes => WebAssembly.instantiate(bytes))
+      // @ts-ignore
+      .then(results => results.instance.exports.greet());
+  }
 }
