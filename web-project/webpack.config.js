8239e2c0d5d37d2d8056b034cad004398493e4e8
Stojan Ragnvlad <stojanragnvlad@gmail.com>
2024-05-23 03:37:25 -0600

    React and webpack now work.

---
+++ web-project/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: { path: path.resolve(__dirname, "dist") },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options:{
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          }
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'src/index.html',
+    }),
+  ],
+};
