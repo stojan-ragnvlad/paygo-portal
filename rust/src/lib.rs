b99a68e233335a63a43a5503ffcf5cc7dfaeab79
Dorian Ferrer <ferrersolutions296@gmail.com>
2024-06-02 11:45:26 -0600

    Removing unneeded code.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,18 +1,7 @@
-use rand::{Rng, thread_rng};
-use rand::distributions::Alphanumeric;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn create_sql_schema_from_csv(csv_table: &[u8]) -> String {
   return std::str::from_utf8(csv_table).unwrap();
-}
-
-#[wasm_bindgen]
-pub fn create_random_string() -> String {
-  let mut rng = thread_rng();
-  let chars: String =
-      (0..24).map(|_| rng.sample(Alphanumeric) as char).collect();
-
-  return chars;
 }
 
