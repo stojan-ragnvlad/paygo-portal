b585d2c194170b7b7d573194c85c236c21570601
Dorian Ferrer <ferrersolutions296@gmail.com>
2024-06-11 22:39:26 -0600

    Updating the start bash script.

--- start.sh
+++ start.sh
@@ -6,30 +6,37 @@ docker volume prune -f
 
 docker image prune -f
 
+docker volume rm static-files
+
 docker volume create static-files
+
+docker volume rm web-src
+
+docker volume create web-src
 
 cd rust
 
 docker build -t paygo-portal/rust:latest .
 
-docker image prune -f
-
-docker run --rm -v static-files:/package paygo-portal/rust:latest
+docker run --rm -v web-src:/package paygo-portal/rust:latest
 
 cd ../web-browser
 
 docker build -t paygo-portal/web-project:latest .
 
-docker image prune -f
-
-docker run --rm -v static-files:/dist paygo-portal/web-project:latest
+docker run --rm \
+  -v static-files:/build/dist \
+  -v web-src:/build/lib \
+  paygo-portal/web-project:latest
 
 cd ../node-ts
 
-docker build -t node-ts-project:latest .
-
-docker image prune -f
+docker build -t paygo-portal/node-ts:latest .
 
 docker run -d -p 127.0.0.1:8080:80/tcp -v static-files:/public\
- node-ts-project:latest
+  paygo-portal/node-ts:latest
+
+docker volume prune -f
+
+docker image prune -f
 
