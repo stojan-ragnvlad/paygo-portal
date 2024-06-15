39dd3e4257bdceb81eb7d8b50c0e6787e02b31e8
Dorian Ferrer <ferrersolutions296@gmail.com>
2024-06-15 11:37:50 -0600

    Figuring out the correct batch size.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,6 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn create_sql_schema_from_csv(csv_table: &[u8]) -> String {
-  return std::str::from_utf8(csv_table).unwrap().to_string();
+  return std::str::from_utf8(&csv_table[0..10]).unwrap().to_string();
 }
 
