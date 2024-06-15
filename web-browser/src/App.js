8523dea3f500ccabc57ab3765c91fa3029c36463
Dorian Ferrer <ferrersolutions296@gmail.com>
2024-06-15 11:16:24 -0600

    Enhancing the app javascript file.

--- web-browser/src/App.js
+++ web-browser/src/App.js
@@ -2,14 +2,54 @@ import React from 'react';
 import init, { create_sql_schema_from_csv, } from '../lib/compile_time_tools';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      worker: new Worker(new URL('worker.js', import.meta.url)),
+      loading: true
+    };
+
+    this.state.worker.onmessage = (event) => {
+      console.log(create_sql_schema_from_csv(new Uint8Array(event.data)).length);
+    };
+  }
+
   componentDidMount() {
-    init().then(() => console.log(create_sql_schema_from_csv([1, 2, 3])));
+    init().then(() => this.setState({ loading: false }));
+  }
+
+  onFileInputChange(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+      this.state.worker.postMessage([file]);
+    }
   }
 
   render() {
+    const elements = [];
+
+    if (this.state.loading) {
+      elements.push((<div key="1">Loading...</div>));
+    } else {
+      elements.push(
+        (
+          <input
+            key="1"
+            type="file"
+            accept="text/csv"
+            onChange={(event) => this.onFileInputChange(event)}
+          />
+        )
+      );
+    }
+
     return (
       <div>
         <h1>React App</h1>
+
+        { elements }
       </div>
     );
   }
