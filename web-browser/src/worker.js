64cd9114159c427a5699a0b3c5321785eb71100d
Dorian Ferrer <ferrersolutions296@gmail.com>
2024-06-15 18:42:22 -0600

    Calling the create sql schema from csv function within the worker.

--- web-browser/src/worker.js
+++ web-browser/src/worker.js
@@ -1,10 +1,20 @@
+import init, { create_sql_schema_from_csv } from '../lib/compile_time_tools';
+
 onmessage = function (event) {
   const reader = new FileReader();
 
-  reader.readAsArrayBuffer(event.data[0]);
+  reader.readAsArrayBuffer(event.data);
 
-  reader.onload =
-    (loadEvent) => postMessage([event.data[0].name, reader.result]);
+  reader.onload = () => {
+    init().then(() => {
+      const sql_query = create_sql_schema_from_csv(
+        event.data.name,
+        new Uint8Array(reader.result)
+      );
+
+      postMessage(sql_query);
+    });
+  }
 
   reader.onerror = (error) => postMessage(error);
 }
