c87dd0c45f8eb38283484a5353e15a61f9d1c701
Dorian Ferrer <ferrersolutions296@gmail.com>
2024-07-20 04:24:21 -0600

    Removing the make directory line.

--- rust/build.sh
+++ rust/build.sh
@@ -7,8 +7,6 @@ then
   cargo add csv@1.3.0
 
   wasm-pack build --dev --target web -d package
-
-  mkdir -p dist && cp -R package/* dist
 elif [ "$PP_BUILD_TYPE" = "executable" ]
 then
   cargo add postgres@0.19.7
